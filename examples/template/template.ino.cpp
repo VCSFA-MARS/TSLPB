e50bd09527a8e8207ace3b88e448fb78a90c7bc4
Nick Counts <nick.counts@gmail.com>
2019-05-03 18:08:58 -0400

    Initial Commit
    Tested library, version 0.3.0. No built-in support for onboard EEPROM

---
+++ examples/template/template.ino.cpp
@@ -0,0 +1,35 @@
+/*  ┌──────────────────────────────────────────────────┐
+    │      VCSFA Thinsat Software Template - 2019      │
+    └──────────────────────────────────────────────────┘
+
+    Description of the project
+    Developed with [embedXcode](http://embedXcode.weebly.com)
+
+    Author       Nicholas Counts
+                 Counts Engineering
+
+    Date         05/02/18 5:56 PM
+    Version      0.2.0
+
+    Copyright    © Nicholas Counts, 2018
+    Licence      MIT
+
+*/
+
+
+/*  ┌──────────────────────────────────────────────────┐
+ *  │  Include Twiggs Space Lab Payload Board Library  │
+ *  └──────────────────────────────────────────────────┘ */
+
+#include "TSLPB.h"
+
+/*  ┌──────────────────────────────────────────────────┐
+ *  │   Include Generic ThinSat DataPacket Structure   │
+ *  └──────────────────────────────────────────────────┘ */
+
+#include "ThinSat_DataPacket_generic.h"
+
+
+
+
+

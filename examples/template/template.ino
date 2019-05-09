2fd2cca687d9196d5089152473c29f3aadb1464c
Nick Counts <nick.counts@gmail.com>
2019-05-09 13:07:54 -0400

    template example compiles

--- examples/template/template.ino
+++ examples/template/template.ino
@@ -9,7 +9,7 @@
                  Counts Engineering
 
     Date         05/02/18 5:56 PM
-    Version      0.2.0
+    Version      0.5.1
 
     Copyright    © Nicholas Counts, 2018
     Licence      MIT
@@ -24,12 +24,33 @@
 #include "TSLPB.h"
 
 /*  ┌──────────────────────────────────────────────────┐
- *  │   Include Generic ThinSat DataPacket Structure   │
+ *  │    Include Custom ThinSat DataPacket Structure   │
  *  └──────────────────────────────────────────────────┘ */
 
-#include "ThinSat_DataPacket_generic.h"
+// #include "myDataPacketStructure.h"
 
+/*  ┌──────────────────────────────────────────────────┐
+ *  │   Instantiate Controller Classes and Variables   │
+ *  └──────────────────────────────────────────────────┘ */
 
+TSLPB pb;
+ThinsatPacket_t data;
 
+void setup()
+{
+    pb.begin();
+    Serial.begin(TSL_DIAGNOSTIC_BAUD);
+}
 
+void loop()
+{
+    // Your code goes here
+    
+    // Example: try to push data to NSL every 5 seconds
+    
+    if (pb.isClearToSend()) {
+        pb.pushDataToNSL(data);
+    }
 
+    delay(5000);
+}
